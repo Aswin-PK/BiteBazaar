aac3caa9322eba9e33cb597a0de181f9f4956c8a
Aswin-PK <aswinpk7737@gmail.com>
2024-03-09 11:58:49 +0530

    feat: added top brands carousel display

--- BiteBazaar/src/Components/Layouts/Top Brand List/TopBrands.jsx
+++ BiteBazaar/src/Components/Layouts/Top Brand List/TopBrands.jsx
@@ -1,9 +1,52 @@
-// import React from 'react'
+// import * as React from "react"
+import brands from "../../../Services/brand-data";
 
-// function TopBrands() {
-//   return (
-//     <div className='flex items-center overflo'></div>
-//   )
-// }
+import { Card, CardContent } from "@/Components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/Components/ui/carousel";
 
-// export default TopBrands
+function TopBrands() {
+  return (
+    <>
+      <div
+        className="heading
+        text-xl font-bold mt-6 lg:mt-10
+        px-5 xl:px-[10rem]
+        ">Top Brands
+      </div>
+      <div className="flex items-center justify-center px-4 lg:px-[10rem]">
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          className="w-full max-w-full"
+        >
+          <CarouselContent>
+            {brands.map((brand, index) => (
+              <CarouselItem key={index} className="basis-1/4 lg:basis-1/6">
+                <div className="p-1 rounded-full">
+                  <Card className="">
+                    <CardContent className="h-[5rem] lg:h-[10rem] w-[5rem] lg:w-[10rem] flex items-center justify-center border aspect-square rounded-full overflow-hidden cursor-pointer">
+                      <div className="brand-image w-[70%] h-[70%] ">
+                        <img src={brand.brand_image} alt="" className="w-full h-full object-cover" />
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </>
+  );
+}
+
+export default TopBrands;
---
+++ BiteBazaar/src/Services/brand-data.js
@@ -0,0 +1,50 @@
+import burgerking from '../assets/img/brand_images/burgerking_logo.png'
+import freshmenu from '../assets/img/brand_images/freshmenu_logo.png'
+import kfc from '../assets/img/brand_images/kfc_logo.png'
+import mcDelivery from '../assets/img/brand_images/mc_delivery_logo.png'
+import truffles from '../assets/img/brand_images/truffles_logo.png'
+
+const brands = [
+    {
+        brand_name: 'KFC',
+        brand_image: kfc
+    },
+    {
+        brand_name: 'Burger King',
+        brand_image: burgerking
+    },
+    {
+        brand_name: 'Truffles',
+        brand_image: truffles
+    },
+    {
+        brand_name: 'Fresh Menu',
+        brand_image:  freshmenu
+    },
+    {
+        brand_name: "McDonald's",
+        brand_image: mcDelivery
+    },
+    {
+        brand_name: 'KFC',
+        brand_image: kfc
+    },
+    {
+        brand_name: 'Burger King',
+        brand_image: burgerking
+    },
+    {
+        brand_name: 'Truffles',
+        brand_image: truffles
+    },
+    {
+        brand_name: 'Fresh Menu',
+        brand_image:  freshmenu
+    },
+    {
+        brand_name: "McDonald's",
+        brand_image: mcDelivery
+    },
+]
+
+export default brands;
